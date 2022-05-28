2a34093e4088c5c6be2202cb2ace0ce484c3a0e7
d0ri123 <lsh81550@gmail.com>
2022-05-28 11:48:01 +0900

    로그인 기능 추가,

--- src/main/java/com/gdsc/book/webservice/web/IndexController.java
+++ src/main/java/com/gdsc/book/webservice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.gdsc.book.webservice.web;
 
+import com.gdsc.book.webservice.config.auth.LoginUser;
+import com.gdsc.book.webservice.config.auth.dto.SessionUser;
 import com.gdsc.book.webservice.service.PostsService;
 import com.gdsc.book.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";// index.mustache 파일이 호출된다
     }
 
